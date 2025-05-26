23f8c10a75720a4b42fcf3d642127f08cb3d8fea
Pedro Silva <pedro.mape7@gmail.com>
2025-05-26 20:24:23 -0300

    first commit

---
+++ src/main/java/com/example/login_auth_api/domain/user/User.java
@@ -0,0 +1,29 @@
+package com.example.login_auth_api.domain.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+}
